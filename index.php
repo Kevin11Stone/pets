bfb8181f3b85318e5902a1c43e73c0fd67936304
Tom Loudon <tloudong@srv603.webhostingforstudents.com>
2023-01-12 17:27:07 -0500

    Added controller page

---
+++ index.php
@@ -0,0 +1,25 @@
+<?php
+//This is my CONTROLLER for the hello project
+
+//Turn on error reporting (This turns on error reporting for every page. because everything has to go through the model (this page))
+ini_set('display_errors', 1);
+error_reporting(E_ALL);
+
+//Require autoload file
+require_once('vendor/autoload.php');
+
+//create an instance of the base class
+$f3 = Base::instance();
+//Java Equivalent === Base f3 = new Base()
+
+
+//Define a default root
+$f3->route('GET /', function(){
+    echo '<h1>Pet Home</h1>';
+//    $view = new Template();
+//    echo $view->render('views/home.html');
+});
+
+//Run Fat-Free
+$f3->run();
+//Java Equivalent ->f3.run()
